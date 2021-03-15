8240c98fd41f9805d17b2a4770fea2e4c5cdd983
WibyFabian08 <wibyfabian08@gmail.com>
2021-03-15 08:11:19 -0700

    Database Model

--- app/Models/Transction.php
+++ app/Models/Transction.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 'food_id', 'quantity', 'total', 'status', 'payment_url'
+    ];
+
+    public function getCreatedAtAttribute($value) {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAtAttribute($value) {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    // membuat relasi
+    public function food() {
+        return $this->hasOne(Food::class, 'id', 'food_id');
+    }
+
+    public function user() {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
